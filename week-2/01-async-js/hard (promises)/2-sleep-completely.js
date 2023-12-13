2ed47e1db1315d930a21c3e7f9c34fd795bc0131
Prakhar Pathak <prakharpathak9450@gmail.com>
2023-12-13 16:44:10 +0530

    Week 2 Assignment

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,14 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+  let currentTime = new Date().getTime();
+  let start = 0;
+  // console.log(currentTime);
+  while (start < seconds) {
+    start = new Date().getTime() - currentTime;
+  }
 }
+console.log("Started busy waiting");
+sleep(2000);
+console.log("Busy wait ended");
--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,19 +4,48 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
-
 function waitOneSecond() {
-
+  let promise1 = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("1 second...");
+    }, 1000);
+  });
+  return promise1;
 }
 
 function waitTwoSecond() {
-
+  let promise2 = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("2 second...");
+    }, 2000);
+  });
+  return promise2;
 }
 
 function waitThreeSecond() {
-
+  let promise3 = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("3 second...");
+    }, 3000);
+  });
+  return promise3;
 }
+
+waitOneSecond().then(function (value) {
+  console.log(value);
+});
+
+waitTwoSecond().then(function (value) {
+  console.log(value);
+});
+
+waitThreeSecond().then(function (value) {
+  console.log(value);
+});
 
 function calculateTime() {
-
+  let d = new Date();
+  console.log(d.getDate());
 }
+
+calculateTime();
